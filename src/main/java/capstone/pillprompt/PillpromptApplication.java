b4e98231b8924dc16e9fa1c4af3eb796f3377230
Hanjaemo <hjm2530@gmail.com>
2023-06-12 02:44:55 +0900

    Initial commit

---
+++ src/main/java/capstone/pillprompt/PillpromptApplication.java
@@ -0,0 +1,13 @@
+package capstone.pillprompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillpromptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillpromptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/pillprompt/PillpromptApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.pillprompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillpromptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
