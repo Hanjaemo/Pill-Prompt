a141e6dd2cbf0a69fc7a14a87f0c9a94f8370c6b
Hanjaemo <hjm2530@gmail.com>
2023-06-20 21:28:30 +0900

    feat: PillRepository 개발

---
+++ src/main/java/capstone/pillprompt/repository/PillRepository.java
@@ -0,0 +1,34 @@
+package capstone.pillprompt.repository;
+
+import capstone.pillprompt.domain.Pill;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PillRepository {
+
+    private final EntityManager em;
+
+    public void save(Pill pill) {
+        em.persist(pill);
+    }
+
+    public Pill findById(Long id) {
+        return em.find(Pill.class, id);
+    }
+
+    public List<Pill> findAll() {
+        return em.createQuery("select p from Pill p", Pill.class)
+                .getResultList();
+    }
+    
+    public void delete(Long id) {
+        Pill pill = em.find(Pill.class, id);
+        em.remove(pill);
+    }
+    
+}
