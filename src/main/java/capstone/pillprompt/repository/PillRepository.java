a3d1590f96291c7066b39559207040a823aa9b31
Hanjaemo <hjm2530@gmail.com>
2023-07-17 13:53:30 +0900

    feat: 복용시간별 Pill 객체 조회

--- src/main/java/capstone/pillprompt/repository/PillRepository.java
+++ src/main/java/capstone/pillprompt/repository/PillRepository.java
@@ -1,12 +1,12 @@
 package capstone.pillprompt.repository;
 
+import capstone.pillprompt.domain.NameOfTime;
 import capstone.pillprompt.domain.Pill;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,13 +19,19 @@ public class PillRepository {
         return pill.getId();
     }
 
-    public Pill findById(Long id) {
-        return em.find(Pill.class, id);
-    }
-
     public List<Pill> findAll() {
         return em.createQuery("select p from Pill p", Pill.class)
                 .getResultList();
+    }
+
+    public List<Pill> findByTime(String time) {
+        return em.createQuery("select p from Pill p join p.times t where t = :timeName", Pill.class)
+                .setParameter("timeName", time)
+                .getResultList();
+    }
+
+    public Pill findById(Long id) {
+        return em.find(Pill.class, id);
     }
 
     public void delete(Long id) {
