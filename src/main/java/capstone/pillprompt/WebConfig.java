42daf2513c05b7a37ada651e82af3c8d7a036927
Hanjaemo <hjm2530@gmail.com>
2023-07-17 13:11:13 +0900

    feat: create WebConfig.class

---
+++ src/main/java/capstone/pillprompt/WebConfig.java
@@ -0,0 +1,20 @@
+package capstone.pillprompt;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final int MAX_AGE_SEC = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(MAX_AGE_SEC);
+    }
+}
