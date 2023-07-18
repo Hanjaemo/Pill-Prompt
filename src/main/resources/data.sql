3868d23223b4bdc50998b72b442edeb490f8847a
Hanjaemo <hjm2530@gmail.com>
2023-07-18 15:07:11 +0900

    feat: 실행시 저장될 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+insert into taking_time values ('08:00:00', 'MORNING');
+insert into taking_time values ('12:45:00', 'LUNCH');
+insert into taking_time values ('18:30:00', 'DINNER');
+
+insert into pill values (0, 12, 1, '감기약');
+insert into pill values (0, 4, 2, '비타민C');
+insert into pill values (0, 21, 3, '타이레놀');
+insert into pill values (0, 5, 4, '오메가3');
+
+insert into name_of_time values (1, 'MORNING');
+insert into name_of_time values (1, 'DINNER');
+insert into name_of_time values (2, 'LUNCH');
+insert into name_of_time values (3, 'MORNING');
+insert into name_of_time values (3, 'LUNCH');
+insert into name_of_time values (3, 'DINNER');
+insert into name_of_time values (4, 'LUNCH');
+
+
