f0b4d524e7352813fa7eb5b93bc4f2cd2136806d
Hanjaemo <hjm2530@gmail.com>
2023-09-24 22:11:15 +0900

    feat(Member): Pill 엔티티와 연관관계 매핑

--- src/main/java/capstone/pillprompt/domain/Member.java
+++ src/main/java/capstone/pillprompt/domain/Member.java
@@ -1,10 +1,14 @@
 package capstone.pillprompt.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Email;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -26,4 +30,7 @@ public class Member {
 
     @Column(nullable = false)
     private String password;
+
+    @OneToMany(mappedBy = "member")
+    private List<Pill> pills = new ArrayList<>();
 }
