9089f6d7a1dd57680c5cf6f2099285ec6a328c15
Hanjaemo <hjm2530@gmail.com>
2023-09-25 15:58:35 +0900

    Revert "feat(Member): Pill 엔티티와 연관관계 매핑"
    This reverts commit f0b4d524e7352813fa7eb5b93bc4f2cd2136806d.

--- src/main/java/capstone/pillprompt/domain/Member.java
+++ src/main/java/capstone/pillprompt/domain/Member.java
@@ -1,14 +1,10 @@
 package capstone.pillprompt.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Email;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -30,7 +26,4 @@ public class Member {
 
     @Column(nullable = false)
     private String password;
-
-    @OneToMany(mappedBy = "member")
-    private List<Pill> pills = new ArrayList<>();
 }
